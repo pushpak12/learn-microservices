959039453eb0c1c15771ee7e1adc07580907e198
Pushpak Katkar <pushpakkatkar@gmail.com>
2024-04-30 13:14:38 +0530

    Completed communication between services using RestTemplate

--- UserService/src/main/java/com/learn/microservice/user/service/services/UserServiceImpl.java
+++ UserService/src/main/java/com/learn/microservice/user/service/services/UserServiceImpl.java
@@ -1,11 +1,21 @@
 package com.learn.microservice.user.service.services;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.learn.microservice.user.service.entities.Hotel;
+import com.learn.microservice.user.service.entities.Rating;
 import com.learn.microservice.user.service.entities.User;
 import com.learn.microservice.user.service.exceptions.ResourceNotFoundException;
 import com.learn.microservice.user.service.repositories.UserRepository;
@@ -15,6 +25,12 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
 	@Override
 	public User saveUser(User user) {
@@ -33,7 +49,33 @@ public class UserServiceImpl implements UserService{
 	@Override
 	public User getUser(String userId) {
 		// TODO Auto-generated method stub
-		return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found "+userId));
+		// get user from database with the help of repository
+		User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User with given id is not found "+userId));
+		// fetch rating of the above user from RATING SERVICE
+		// http://localhost:8083/ratings/users/5b95938a-3d37-4948-a4ce-196c5124086e
+		
+		Rating[] ratingsOfUser = restTemplate.getForObject("http://RATINGSERVICE/ratings/users/"+user.getUserId(), Rating[].class);
+		
+		
+		List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
+		logger.info("{}", ratings);
+		
+		List<Rating> ratingList =  ratings.stream().map(rating -> {
+			// api call to Hotel Service to get the hotel
+			// http://localhost:8082/hotels/d7d0d468-422e-4147-9174-915f46ba2c2c
+			ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTELSERVICE/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel = forEntity.getBody();
+			logger.info("response status code",forEntity.getStatusCode());
+			
+			// set hotel to rating
+			rating.setHotel(hotel);
+			
+			// return rating
+			return rating;
+		}).collect(Collectors.toList());
+		
+		user.setRatings(ratings);
+		return user;
 	}
 
 }
